8b3268b80138f1543b6fd34ddb2fbda1b7fd4ff6
fernangit <morio.tsunaki@gmail.com>
2019-12-08 15:53:47 +0900

    modify

--- supersonicPlug.py
+++ supersonicPlug.py
@@ -2,31 +2,49 @@
 import tplink_smartplug_py3 as plug
 import supersonic as sonic
 import jtalk as jt
+"""
+自動判別の場合 
+$ sudo amixer cset numid=3 0
+ライン出力の場合 
+$ sudo amixer cset numid=3 1
+HDMI出力の場合 
+$ sudo amixer cset numid=3 2
+"""
 
 def main()
     GPIO_TRIG = 21
     GPIO_ECHO = 20
     DIS_THRESH = 50 #50cm
+    ON_COUNT = 5 #10sec
+    OFF_COUNT = 15 #30sec
 
-    var plugStatus = ON
     sonic.init_sensors(GPIO_TRIG, GPIO_ECHO)
+    var plugStatus = OFF
+    var onCount = 0
+    var offCount = 0
     while True:
         var distance = sonic.get_distance(GPIO_TRIG, GPIO_ECHO)
         if distance < DIS_THRESH:
-            if plugStatus == OFF:
+            offCount = 0
+            onCount++
+            if onCount >= ON_COUNT and plugStatus == OFF:
                 #Plug ON
+                onCount = 0
                 plugStatus = ON
                 plug.control('192.168.0.2', 'on')
-                jt.jtalk('いらっしゃいっ')
+                jt.jtalk('いらっしゃい')
         else:
-            if plugStatus == ON:
+            onCount = 0
+            offCount++
+            if offCount >= OFF_COUNT and plugStatus == ON:
                 #Plug OFF
+                offCount = 0
                 plugStatus = OFF
                 plug.control('192.168.0.2', 'off')
-                jt.jtalk('またねっ')
+                jt.jtalk('またね')
 
         print("距離：{0} cm".format(distance)
-        time.sleep(2)
+        time.sleep(2) #2sec wait
 
 
 if __name__ == "__main__":
